fc80752173fa804702dd3056fa691d9b6af084ab
HeJing <JingHe6@JingHe6.com>
2018-02-02 13:46:44 +0800

    Initial Commit

---
+++ aaaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaaa
+//
+//  Created by 何静 on 2018/2/2.
+//  Copyright © 2018年 HongTongRongBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaaa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aaaaa
+//
+//  Created by 何静 on 2018/2/2.
+//  Copyright © 2018年 HongTongRongBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
